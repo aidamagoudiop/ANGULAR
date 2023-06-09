076d9f56b2e4956b4df25636a3f9e94165b13139
aidamagoudiop <aidamagoudiop40@gmail.com>
2023-06-09 20:31:38 +0000

    creation des cartes dde sacs et des pages login et register

--- SUNUSHOP/src/app/app.module.ts
+++ SUNUSHOP/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ProduitsComponent } from './components/produits/produits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    ProduitsComponent
   ],
   imports: [
     BrowserModule,
